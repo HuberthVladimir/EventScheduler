81e4c9d10ba3de60bc506267d333fab32f407a56
HuberthVladimir <vladimirhuberth@hotmail.com>
2024-12-07 20:15:23 -0300

    feat: entity Categoria created

---
+++ src/main/java/com/huberthvladimir/EventScheduler/entities/Categoria.java
@@ -0,0 +1,43 @@
+package com.huberthvladimir.EventScheduler.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    public Categoria() {}
+
+    public Categoria(String descricao, Integer id) {
+        this.descricao = descricao;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+}
